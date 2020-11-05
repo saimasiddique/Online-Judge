bd571578e37a55d7676b6b362c2f5a86e9fffd4c
Saima Siddique Tashfia <siddiquesaima16022@gmail.com>
2020-11-06 03:34:47 +0600

    Add files via upload

---
+++ Light OJ/1000 - Greetings from LightOJ.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,t,i;
+    while(scanf("%d",&t)==1)
+    {
+       for(i=1;i<=t;i++)
+       {
+           scanf("%d %d",&a,&b);
+           printf("Case %d: %d\n",i,a+b);
+       }
+    }
+    return 0;
+}
